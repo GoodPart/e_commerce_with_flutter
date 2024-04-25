ee269fd44be406949f0c80af8f71f3e5085e65b9
GoodPart <40373934+GoodPart@users.noreply.github.com>
2024-04-25 16:58:17 +0900

    git first push

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+
+
+import 'presentation/pages/home/home_page.dart';
+import 'presentation/pages/splash/splash_page.dart';
+import 'presentation/routes/routes.dart';void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig:router,
+    );
+    // return MaterialApp(
+    //   home: Scaffold(
+    //     body: Center(
+    //       child: Text("hello"),
+    //     ),
+    //   ),
+    // );
+  }
+}
+
